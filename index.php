0707389331a2a9f9356ee3abd04901bed60d665c
env107 <env107@126.com>
2020-02-16 18:31:05 +0800

    init code

--- index.php
+++ index.php
@@ -7,8 +7,11 @@ ini_set("display_errors","on");
 require_once 'autoload.php';
 
 try {
-//    SignatureCheckerTest::index();
-      SessionTest::index();
+    //==========================================
+    // 请去掉需要测试的模块代码
+        SignatureCheckerTest::index();   //认证签名
+//        SessionTest::index(); //会话管理
+    //===========================================
 } catch (Exception $exception) {
     echo $exception->getMessage()." At ".$exception->getFile()." On ".$exception->getLine()."(".get_class($exception).")";
     echo "<pre>";
